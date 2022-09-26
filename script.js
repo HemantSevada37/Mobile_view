aca0ad0c72f6470b152a509d1ce74ab70a3e8020
HemantSevada37 <102539246+HemantSevada37@users.noreply.github.com>
2022-09-26 09:14:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+        function fun(x){
+            if(x== 1){
+                // document.getElementById('test').innerHTML = "random";
+                document.getElementById('photo').style.
+                backgroundImage = "url(photos/home.jpg)";
+            }else if(x==2){
+                document.getElementById('photo').style.
+                backgroundImage = "url(photos/work.jpg)";
+            }else if(x==3){
+                document.getElementById('photo').style.
+                backgroundImage = "url(photos/blog.jpg)";
+            }else if(x==4){
+                document.getElementById('photo').style.
+                backgroundImage = "url(photos/about.jpg)";
+            }
+        }
+        
